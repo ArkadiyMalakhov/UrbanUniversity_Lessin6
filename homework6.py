ad00869d2c1a1a3a7fc022d8c65fe3901946c1be
AMalakhov <spamarkady@yandex.ru>
2024-05-24 10:10:21 +0300

    Update homework6.py

--- homework6.py
+++ homework6.py
@@ -1,6 +1,17 @@
+print('----СПИСКИ----')
+
 my_list = ['Apple', 'Pineapple', 'Banana', 'Coconut', 'Peach']
 print(my_list)
 print(my_list[0], my_list[-1])
 print(my_list[2:])
 my_list[2] = 'Grape'
 print(my_list)
+
+print('----СЛОВАРИ----')
+
+my_dict = {'apple': 'яблоко', 'banana': 'банан', 'orange': 'апельсин'}
+print(my_dict)
+print(my_dict['banana'])
+print('Дабавление нового ключа и значения Киви:"')
+my_dict['Kiwi'] = 'киви'
+print(my_dict)
